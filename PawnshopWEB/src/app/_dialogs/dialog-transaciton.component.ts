26b9afb094defb8a729aa22b79ca0756345eb84b
eduard0708 <eduard.uytoco@gmail.com>
2021-09-03 13:16:09 +0300

    update new pawner

--- PawnshopWEB/src/app/_dialogs/dialog-transaciton.component.ts
+++ PawnshopWEB/src/app/_dialogs/dialog-transaciton.component.ts
@@ -1,5 +1,11 @@
 import { AfterContentInit, Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Pawner } from '../_model/pawner';
 import { TestService } from '../_service/test.service';
@@ -7,66 +13,71 @@ import { DialogNewpawnerComponent } from './dialog-newpawner.component';
 
 @Component({
   selector: 'app-dialog-transaciton',
-  templateUrl: './dialog-transaciton.component.html'
+  templateUrl: './dialog-transaciton.component.html',
 })
 export class DialogTransacitonComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public transactionType:any,
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public transactionType: any,
     private dialogRef: MatDialogRef<DialogTransacitonComponent>,
     private testService: TestService,
-    private router:Router,
-    private dialog: MatDialog
-  ) { }
-
-  contactNumber:string;
-  pawners:Pawner;
+    private router: Router,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
+  contactNumber: string;
+  pawners: Pawner;
+  existingPawner:Pawner[]=[];
   buttonNewloan = false;
-  placeHolder = "Enter Transaction Number"
-  
+  placeHolder = 'Enter Transaction Number';
+
   ngOnInit(): void {
-    if(this.transactionType === "New Loan")
-    {
+    if (this.transactionType === 'New Loan') {
       this.buttonNewloan = true;
-      this.placeHolder = "Contact Number";
+      this.placeHolder = 'Contact Number';
     }
-     
   }
 
-  search(){
-    this.contactNumber ='';
+  search() {
+    this.contactNumber = '';
   }
 
-  cancel(){
+  cancel() {
     this.dialogRef.close();
-  
   }
 
-  findPawner(){
-      this.testService.findPawner().subscribe(pawners => {
-      
-        
-         for(var pawner of pawners){
-          if(pawner.contactNumber.toString() === this.contactNumber.toString()) 
-          {
-            console.log(pawner);
-            this.router.navigateByUrl('transactions/newloan');
-            this.cancel();
-          }
-          
+  findPawner() {
+    this.testService.findPawner().subscribe((pawners) => {
+      for (var pawner of pawners) {
+        if (pawner.contactNumber.toString() === this.contactNumber.toString()) {
+          this.existingPawner.push(pawner)
+        }
+      }
+    }, error => console.log(error),
+    () => this.checkPawnerExist(this.existingPawner)
+    );   
+  }
 
-         }
-          
-      })
-  }  
+  checkPawnerExist(pawners:Pawner[]) {
+    if(pawners.length == 0) {
+      this.snackBar.open('NO contact pawner exist!', 'OK',{duration:1000});
+      return;
+    }
+    
+    if(pawners.length == 1){
+      this.router.navigateByUrl('/transactions/newloan');
+      this.dialogRef.close();
+    } else {
+      this.dialogRef.close();
+    }  
+  }
 
-  createPawer(){
+  createPawer() {
     this.dialogRef.close();
     const config = new MatDialogConfig();
     config.position = { top: '5rem' };
-    config.width='75%';
+    config.width = '75%';
     config.disableClose = true;
-  
-    this.dialog.open(DialogNewpawnerComponent, config)
-  }
 
+    this.dialog.open(DialogNewpawnerComponent, config);
+  }
 }
