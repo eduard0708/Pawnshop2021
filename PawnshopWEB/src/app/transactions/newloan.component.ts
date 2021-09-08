0b0d83b851658ba1270f980979e83ad88ef17c36
eduard0708 <eduard.uytoco@gmail.com>
2021-09-08 15:36:07 +0300

    update date

--- PawnshopWEB/src/app/transactions/newloan.component.ts
+++ PawnshopWEB/src/app/transactions/newloan.component.ts
@@ -1,47 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pawner } from '../_model/pawner';
-import * as _moment from 'moment';
-import { DatePipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-newloan',
-  templateUrl: './newloan.component.html'
+  templateUrl: './newloan.component.html',
+
 })
 export class NewloanComponent implements OnInit {
 
-  pawner:Pawner = {} as Pawner;
-  formDates:FormGroup;
-  today =  Date.now()
+  pawner: Pawner = {} as Pawner;
+  formDates: FormGroup;
+  today = new Date();
+  dateMature =  new Date( new Date().setMonth( new Date().getMonth() + 1));
+  dateExpire = new Date( new Date().setMonth( new Date().getMonth() + 4))
+  
+constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
 
-  constructor(private activatedRoute: ActivatedRoute, private router:Router, 
-    private fb:FormBuilder,
-    private datePipe: DatePipe
-    ) {
-    
-    const moment = _moment;
-    
-    const today = Date.now();
-    
-    this.activatedRoute.queryParams.subscribe( params => {
-      if(this.router.getCurrentNavigation().extras.state) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
         this.pawner = this.router.getCurrentNavigation().extras.state.pawner
       }
     });
 
-    this.formDates =  fb.group({
-      dateTransaction: []
+    this.formDates = fb.group({
+      dateTransaction: [ this.today],
+      dateMature:[ this.dateMature],
+      dateExpired:[this.dateExpire ]
     });
-    
-   }
+
+  }
 
   ngOnInit(): void {
-    if(this.pawner)
-    console.log('new panwer: '+ JSON.stringify(this.pawner));
-    console.log(this.datePipe.transform(this.today, "yyyy-MM-dd"));
+
+    if (this.pawner)
+      console.log('new panwer: ' + JSON.stringify(this.pawner));
     
   }
-  
+
 }
