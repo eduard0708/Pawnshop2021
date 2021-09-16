6501f4d4485f6ae6dc7c6c842d2234eab7bc1c5c
eduard0708 <eduard.uytoco@gmail.com>
2021-09-16 14:21:46 +0300

    udpate

--- PawnshopWEB/src/app/_dialogs/dialog-newemployee.component.ts
+++ PawnshopWEB/src/app/_dialogs/dialog-newemployee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Select } from '../_model/select';
 
 @Component({
   selector: 'app-dialog-newemployee',
@@ -11,7 +12,10 @@ export class DialogNewemployeeComponent implements OnInit {
 
   employeeForm:FormGroup;
   isSave:boolean = false;
-
+  positions:Select[]=[
+    {"id":1, "name":"Teller"},
+    {"id":2, "name":"Casher"}
+  ];
 
   constructor(
     private dialogRef:MatDialogRef<DialogNewemployeeComponent>
