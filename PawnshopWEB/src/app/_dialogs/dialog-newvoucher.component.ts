c0a4a98f0eccc1de938950a45792a0fcbc445397
eduard0708 <eduard.uytoco@gmail.com>
2021-09-16 06:51:39 +0300

    update

--- PawnshopWEB/src/app/_dialogs/dialog-newvoucher.component.ts
+++ PawnshopWEB/src/app/_dialogs/dialog-newvoucher.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { createMask } from '@ngneat/input-mask';
 import { Select } from '../_model/select';
 import { VoucherService } from '../_service/voucher.service';
 
@@ -11,11 +13,22 @@ import { VoucherService } from '../_service/voucher.service';
   
 })
 export class DialogNewvoucherComponent implements OnInit {
-
+@ViewChild('f') private form:NgForm
   voucherForm:FormGroup;
   transactionCodes:Select[]=[];
   cashCheques:Select[]=[];
   trasactionTypes:Select[]=[];
+  isBtnSave:boolean = true;
+
+  currencyInputMask = createMask({
+    alias: 'numeric',
+    groupSeparator: ',',
+    digits: 2,
+    digitsOptional: false,
+    prefix: '₱ ',
+    placeholder: '0',
+  })
+
   
   constructor(
     private fb: FormBuilder,
@@ -29,22 +42,51 @@ export class DialogNewvoucherComponent implements OnInit {
         transactionType:[Validators.required],
         cashCheque:[Validators.required],
         amount:['',Validators.required],
-        remarks:['',],
+        remarks:['', Validators.required]
       }
     )
-
   }
 
   ngOnInit(): void {
+    console.log(this.voucherForm.valid);   
     this.voucherService.getCashCheque().subscribe( cash => this.cashCheques = cash as any);
     this.voucherService.getTransactionCode().subscribe( code => this.transactionCodes = code as any);
     this.voucherService.getTransactionType().subscribe( type => this.trasactionTypes = type as any);
+
+    this.voucherForm.statusChanges.subscribe(()=> 
+    {
+      this.isBtnSave = !this.voucherForm.valid;      
+      console.log(this.voucherForm.valid);  
+    })
   }
 
   addVoucher(){
 
   }
 
+  reset(){
+
+    Object.keys(this.voucherForm.controls).forEach(keys =>{
+      this.voucherForm.get(keys).setErrors(null);
+      if(keys === 'amount'){
+        console.log('not amount');
+      }else this.voucherForm.get(keys).reset();
+    });
+
+     this.voucherForm.controls.transactionDate.setValue(new Date());
+    // this.voucherForm.controls.transactionCode.setValue('');
+    // this.voucherForm.controls.transactionType.setValue('');
+    // this.voucherForm.controls.cashCheque.setValue('');
+    // this.voucherForm.controls.amount.setValue(0);
+    this.voucherForm.controls.remarks.setValue('');
+    
+    this.voucherForm.controls.remarks.setErrors(null)
+    this.voucherForm.controls.remarks.updateValueAndValidity()
+  
+    console.log(this.voucherForm.valid);  
+      
+
+  }
   close(){
     this.dialogRef.close()
   }
