1b793a594b264968797cbadf0e60d183a40f8a67
eduard0708 <eduard.uytoco@gmail.com>
2021-09-19 12:14:02 +0300

    update login

--- PawnshopAPI/DTO/AddEmployeeDto.cs
+++ PawnshopAPI/DTO/AddEmployeeDto.cs
@@ -9,10 +9,15 @@ namespace PawnshopAPI.Controllers
     public class AddEmployeeDto
     {
         public int Id { get; set; }
+
         [Required]
         public string UserName { get; set; }
+
         [Required]
         public string Password { get; set; }
-    
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
