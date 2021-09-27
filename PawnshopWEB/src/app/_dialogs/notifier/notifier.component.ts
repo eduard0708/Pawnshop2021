5a7a98fb503ea38ab9b3928dcba6894b277da410
eduard0708 <eduard.uytoco@gmail.com>
2021-09-27 07:43:30 +0300

    upsate

--- PawnshopWEB/src/app/_dialogs/notifier/notifier.component.ts
+++ PawnshopWEB/src/app/_dialogs/notifier/notifier.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-notifier',
@@ -8,7 +8,9 @@ import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 })
 export class NotifierComponent implements OnInit {
 
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data:any) { }
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data:any,
+    public snackBarRef:MatSnackBarRef<NotifierComponent>
+  ) { }
 
   ngOnInit(): void {
   }
--- PawnshopWEB/src/app/settings/pawner.component.ts
+++ PawnshopWEB/src/app/settings/pawner.component.ts
@@ -5,11 +5,14 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { NotifierComponent } from '../_dialogs/notifier/notifier.component';
 import { Barangay } from '../_model/barangay';
 import { City } from '../_model/city';
 import { AddressService } from '../_service/address.service';
 import { DialogsService } from '../_service/dialogs.service';
+import { NotifierService } from '../_service/notifier.service';
 import { PawnerService } from '../_service/pawner.service';
 
 @Component({
@@ -30,7 +33,9 @@ export class PawnerComponent implements OnInit {
     private fb: FormBuilder,
     private router: Router,
     private dialogService: DialogsService,
-    private addressService: AddressService
+    private addressService: AddressService,
+    private notifierService: NotifierService,
+    private notifier: MatSnackBar
   ) {
     this.pawnerForm = fb.group({
       firstName: ['', Validators.required],
@@ -45,6 +50,8 @@ export class PawnerComponent implements OnInit {
   serializedDate = new FormControl();
 
   ngOnInit(): void {
+    this.notifierService.error('ONerdfasdf fasdfdf afasdf', '');
+
     setTimeout(() => {
       this.firstNameRef.nativeElement.focus();
     }, 100);
@@ -53,16 +60,15 @@ export class PawnerComponent implements OnInit {
       this.isSave = !this.pawnerForm.valid;
     });
 
-    this.addressService.getCitiesWithBarangays().subscribe(city => {
+    this.addressService.getCitiesWithBarangays().subscribe((city) => {
       this.cities = city as any;
-    })
- }
+    });
+  }
 
-  onSelectCity(){
-      const cityId = +this.pawnerForm.controls.city.value;
-      let city:City = this.cities.find(o => o.cityId === cityId );
-      if(city)
-        this.barangays = city.barangays
+  onSelectCity() {
+    const cityId = +this.pawnerForm.controls.city.value;
+    let city: City = this.cities.find((o) => o.cityId === cityId);
+    if (city) this.barangays = city.barangays;
   }
 
   cancel() {
@@ -77,8 +83,8 @@ export class PawnerComponent implements OnInit {
   save() {
     const empId = JSON.parse(localStorage.getItem('user'));
     const p = this.pawnerForm.value;
-    const city:City = this.cities.find(c => c.cityId === +p.city)
-  
+    const city: City = this.cities.find((c) => c.cityId === +p.city);
+
     //convert address to addresses
     const address = {
       cityName: city.cityName,
@@ -101,13 +107,13 @@ export class PawnerComponent implements OnInit {
       employeeId: empId.id,
       isActive: true,
     };
-    
+
     //call service to add new panwer if add success redirect to newloan
-    this.pawnerService
-      .addPawner(pawner)
-      .subscribe((newPawner) => {
-        if(Object.keys(newPawner).length > 0)
-        this.router.navigateByUrl('transactions/newloan/', {state:{pawner:newPawner}})
-      });
+    this.pawnerService.addPawner(pawner).subscribe((newPawner) => {
+      if (Object.keys(newPawner).length > 0)
+        this.router.navigateByUrl('transactions/newloan/', {
+          state: { pawner: newPawner },
+        });
+    });
   }
 }
